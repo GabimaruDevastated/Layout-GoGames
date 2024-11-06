5b4bd1c94f53454fa945eac7ba76c0b21033a5e2
Gabimaru <goukilya5@gmail.com>
2024-11-06 23:40:51 +0700

    feat: сделал мобильную шапку
    исправлены некоторые стили

--- public/script.js
+++ public/script.js
@@ -1,5 +1,15 @@
-headerButton = document.querySelector('.button_burger');
+const body = document.querySelector('body'),
+    header = document.querySelector('header'),
+    headerButton = document.querySelector('.button_burger'),
+    nav = document.querySelector('.nav');
 
 headerButton.addEventListener('click', e => {
-    
+    headerButton.classList.toggle('burger_active')
+    nav.classList.toggle('nav_active')
+    body.classList.toggle('lock-screen')
+    if (header.hasAttribute('style', 'padding-right: 17px')) {
+        header.removeAttribute('style', 'padding-right: 17px')
+    } else {
+        header.setAttribute('style', 'padding-right: 17px')
+    }
 })
