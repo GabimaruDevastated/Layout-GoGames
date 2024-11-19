eff4b56f4629dc6521040f92af8c18bbda889fe6
Gabimaru <goukilya5@gmail.com>
2024-11-19 07:25:56 +0700

    Оптимизация и совершенствование кода

--- public/script.js
+++ public/script.js
@@ -1,15 +1,21 @@
-const body = document.querySelector('body'),
-    header = document.querySelector('header'),
-    headerButton = document.querySelector('.button_burger'),
-    nav = document.querySelector('.nav');
+const body = document.querySelector("body"),
+    header = body.querySelector("header"),
+    headerButton = header.querySelector(".button_burger"),
+    nav = header.querySelector(".nav"),
+    navItem = nav.querySelectorAll(".item__link");
 
-headerButton.addEventListener('click', e => {
-    headerButton.classList.toggle('burger_active')
-    nav.classList.toggle('nav_active')
-    body.classList.toggle('lock-screen')
-    if (header.hasAttribute('style', 'padding-right: 17px')) {
-        header.removeAttribute('style', 'padding-right: 17px')
-    } else {
-        header.setAttribute('style', 'padding-right: 17px')
-    }
-})
+headerButton.addEventListener("click", () => {
+    headerButton.classList.toggle("burger_active");
+    nav.classList.toggle("nav_active");
+    body.classList.toggle("lock-screen");
+    header.toggleAttribute("style", "padding-right: 17px")
+});
+
+navItem.forEach((item) => {
+    item.addEventListener("click", () => {
+        headerButton.classList.remove("burger_active");
+        nav.classList.remove("nav_active");
+        body.classList.remove("lock-screen");
+        header.removeAttribute("style", "padding-right: 17px");
+    });
+});
